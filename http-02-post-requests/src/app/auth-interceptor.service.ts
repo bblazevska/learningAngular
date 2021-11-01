6d87c2ea8f52b14d6fdfcc5177de22c0e1093894
kuzeva <kuzeva01@gmail.com>
2021-11-01 17:12:37 +0100

    modifying request objects

--- http-02-post-requests/src/app/auth-interceptor.service.ts
+++ http-02-post-requests/src/app/auth-interceptor.service.ts
@@ -4,7 +4,7 @@ export class AuthInterceptorService implements HttpInterceptor{
 
    intercept(req: HttpRequest<any>, next: HttpHandler) {
       console.log('Request is on its way');
-
-      return next.handle(req);
+      const modifiedReq = req.clone({headers: req.headers.append('Auth','xyz')})
+      return next.handle(modifiedReq);
    }
 }
